f99f97e7e83e86241fb21c3b9b1e0d893fc04c5f
reid <reidbras@gmail.com>
2021-09-06 14:14:01 -0500

    Adding Person class to script

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+class Person {
+    constructor(name, pets, residence, hobbies) {
+        this.name = name;
+        this.pets = pets;
+        this.residence = residence;
+        this.hobbies = hobbies;
+    }
+    info() {
+        consile.log(`${this.name}\n${this.pets}\n${this.residence}\n${this.hobbies}`);
+    }
+}
